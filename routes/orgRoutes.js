e14043c24cdd2c93736d3b7cdb286042bdc4b275
boxboxMN <mariel005hernandez@gmail.com>
2026-07-10 18:25:52 +0800

    partial

--- routes/orgRoutes.js
+++ routes/orgRoutes.js
@@ -7,6 +7,49 @@ const router = express.Router();
 router.get("/dashboard", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/organization/dashboard.html"));
 });
+
+router.get("/pending", (req, res) => {
+
+    if (!req.session.accountId) {
+        return res.redirect("/auth/login.html");
+    }
+
+    res.sendFile(
+        path.join(__dirname, "../public/organization/orgPending.html")
+    );
+});
+
+async function checkOrganizationApproval(req, res, next) {
+
+    if (!req.session.accountId) {
+        return res.redirect("/auth/login.html");
+    }
+
+    const [rows] = await pool.query(
+        `SELECT status
+         FROM accounts
+         WHERE account_id=?`,
+        [req.session.accountId]
+    );
+
+    if (!rows.length) {
+        return res.redirect("/auth/login.html");
+    }
+
+    if (rows[0].status === "pending") {
+
+        // If they're trying to access anything except /org/pending
+        if (req.path !== "/pending") {
+            return res.redirect("/org/pending");
+        }
+
+    }
+
+    next();
+
+}
+router.use(checkOrganizationApproval);
+
 router.get("/pets", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/organization/pets.html"));
 });
@@ -31,6 +74,5 @@ router.get("/settings", (req, res) => {
 router.get("/support", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/organization/support.html"));
 });
-
 
 module.exports = router;
